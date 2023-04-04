57f00c1feca5f96d95e0ce87c2e32d33f2e78822
duypr7887 <121009045+duypr7887@users.noreply.github.com>
2023-04-04 16:37:32 +0700

    change

---
+++ list_p2.py
@@ -0,0 +1,20 @@
+# list trong list
+friends = [["Duy",35],["tuyết",28],["Thanh",23]]
+print(type(friends[0]))
+
+# copy list
+list1 = [1,2,3]
+list2 = list1.copy()
+# is
+# id
+print(id(list1),id(list2))
+print(list1 is list2) 
+print(list1 == list2)
+
+# list slicing (lấy ra 1 phần trong list ban đầu  thành list mới)
+a = [1,3,10,1000,45]
+new_list = a [0:2:1]
+
+print(new_list is a)
+print(new_list)
+
--- test.py
+++
@@ -1,5 +0,0 @@
-list1 = [1,2,3]
-list2 = list1.copy()
-
-print(list1 is list2) 
-print(list1 == list2)
